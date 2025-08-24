fc00822398d2d85c05968a8390c9a495ec6f9b2e
yupsang1719 <yupsang1719@outlook.com>
2025-08-24 11:22:04 +0100

    count down changed

--- client/src/components/EventCountdownBanner.jsx
+++ client/src/components/EventCountdownBanner.jsx
@@ -15,7 +15,7 @@ export default function CountdownBanner({ onTicketClick }) {
   }, []);
 
   function getTimeRemaining() {
-    const eventDate = new Date("2025-09-5T20:00:00");
+    const eventDate = new Date("2025-09-05T20:00:00");
     const now = new Date();
     const total = eventDate - now;
     const seconds = Math.floor((total / 1000) % 60);
